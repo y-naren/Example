050c569f0b1c1dcaebd5b61d76bca83e1189db46
Narendra <y.naren00@gmail.com>
2023-09-07 11:30:51 +0530

    Added:program to print prime number:

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-print("hellow world")
-x=4 
-y=4
-print(x+y)
-x=5
-y=2
-print(x-y)
-print(10+5)
-x=99
-y=101
-print(x*y)
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number <= 3:
+        return True
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def main():
+    print("Prime numbers between 1 and 100:")
+    for num in range(1, 101):
+        if is_prime(num):
+            print(num, end=" ")
+
+if __name__ == "__main__":
+    main()
